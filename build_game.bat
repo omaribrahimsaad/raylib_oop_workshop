c81880db6f4cbfd22c14e0ff389a200a81e7c1de
Omar Ibrahim <omaribrahimsaad@gmail.com>
2025-11-09 20:52:56 +0800

    added build script for the final demo.

---
+++ build_game.bat
@@ -0,0 +1,3 @@
+g++ .\game.cpp -o game.exe -I libs/raylib/include -L libs/raylib/lib -lraylib -lopengl32 -lgdi32 -lwinmm -std=c++20 -O0 -g
+game.exe
+
---
+++ build_shape_collision.bat
@@ -0,0 +1,3 @@
+g++ .\shape_collision_system.cpp -o shape_collision_system.exe -I libs/raylib/include -L libs/raylib/lib -lraylib -lopengl32 -lgdi32 -lwinmm -std=c++20 -O0 -g
+shape_collision_system.exe
+
